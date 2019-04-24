dd3899c778870c504ba5bbc2385ce4ca87aeec83
0xc <48725664+00xc@users.noreply.github.com>
2019-04-24 10:53:43 +0200

    Fix error handling

--- h2buster.py
+++ h2buster.py
@@ -1,124 +1,125 @@
 #coding=utf-8
 
 __author__ = "https://github.com/00xc/"
-__version__ = "0.1"
+__version__ = "0.1a"
 
-try:
-	import hyper
-	import ssl, sys, time
+import hyper
+import ssl, sys, time
 
-	# Parse target and check if its HTTPS
-	def check_tls(ip):
-		ip = ip.split("://")
-		if len(ip) == 1:
-			s = 0
-			ip = ip[0]
-		else:
-			if ip[0] == "http": s = 0
-			elif ip[0] == "https": s = 1
-			else: sys.exit("[-] Target not understood")
-			ip = ip[1]
+# Parse target and check if its HTTPS
+def check_tls(ip):
+	ip = ip.split("://")
+	if len(ip) == 1:
+		s = 0
+		ip = ip[0]
+	else:
+		if ip[0] == "http": s = 0
+		elif ip[0] == "https": s = 1
+		else: sys.exit("[-] Target not understood")
+		ip = ip[1]
 
-		return s, ip
+	return s, ip
 
-	# Connect to target and return connection object
-	def h2_connect(ip, s):
-		# Get port
-		if len(ip.split(":")) == 1:
-			if s==1: port=443
-			elif s==0: port=80
-		else: port = int(ip.split(":")[-1])
-		# Perform connection
-		if s == 1:
-			ctx = ssl.SSLContext()
-			ctx.set_alpn_protocols(['h2'])
-			ctx.verify_mode = ssl.CERT_NONE
-			conn = hyper.HTTP20Connection(ip, port=port, ssl_context=ctx, enable_push=False)
-		elif s == 0:
-			conn = hyper.HTTP20Connection(ip, port=port, enable_push=False)
-		# Test connectivity before starting the scan
-		conn.ping("00000000")
+# Connect to target and return connection object
+def h2_connect(ip, s):
+	# Get port
+	if len(ip.split(":")) == 1:
+		if s==1: port=443
+		elif s==0: port=80
+	else: port = int(ip.split(":")[-1])
+	# Perform connection
+	if s == 1:
+		ctx = ssl.SSLContext()
+		ctx.set_alpn_protocols(['h2'])
+		ctx.verify_mode = ssl.CERT_NONE
+		conn = hyper.HTTP20Connection(ip, port=port, ssl_context=ctx, enable_push=False)
+	elif s == 0:
+		conn = hyper.HTTP20Connection(ip, port=port, enable_push=False)
+	# Test connectivity before starting the scan
+	conn.ping("00000000")
 
-		return conn
+	return conn
 
-	# Read responses, print results and return found directories
-	def dump_scan(requests):
+# Read responses, print results and return found directories
+def dump_scan(requests):
 
-		# List of directories that didn't return 404
-		o = list()
+	# List of directories that didn't return 404
+	o = list()
 
-		for key, element in requests.items():
-			resp = conn.get_response(element)
-			status = resp.status
-			#resp.read(decode_content=True) # This seems to improve performance
-			resp.close()
+	for key, element in requests.items():
+		resp = conn.get_response(element)
+		status = resp.status
+		#resp.read(decode_content=True) # This seems to improve performance
+		resp.close()
 
-			# Print meaningful results
-			if status != 404:
-				if key[-1] == "/":
-					o.append(key)
-					ftype = " <DIR>"
-				else: ftype = ""
-				print("[+] " + key + ": " + str(status) + ftype)
+		# Print meaningful results
+		if status != 404:
+			if key[-1] == "/":
+				o.append(key)
+				ftype = " <DIR>"
+			else: ftype = ""
+			print("[+] " + key + ": " + str(status) + ftype)
 
-				# Print redirection location?
-				#if status == 301: print(resp.headers)
-		return o
+			# Print redirection location?
+			#if status == 301: print(resp.headers)
+	return o
 
-	# Scan directory over connection "conn" with dictionary "file". Admits recursion.
-	def recursive_dirscan(conn, directory, file, ext):
+# Scan directory over connection "conn" with dictionary "file". Admits recursion.
+def recursive_dirscan(conn, directory, file, ext):
 
-		i = 0
-		requests = dict()
-		found = list()
+	i = 0
+	requests = dict()
+	found = list()
 
-		l = ["|", "/", "-", "\\"]
-		t = 0
+	l = ["|", "/", "-", "\\"]
+	t = 0
 
-		with open(file, "r") as f:
-			
-			# Main loop
-			for entry in f:
-				entry = entry.rstrip()
+	with open(file, "r") as f:
+		
+		# Main loop
+		for entry in f:
+			entry = entry.rstrip()
 
-				if entry == "/" or entry=="": continue
+			if entry == "/" or entry=="": continue
 
-				for ex in ext:
-					# Prevent flood
-					time.sleep(0.05)
-					# Rotating bar
-					if int(time.time())%5==0:
-						print(l[t], end="\r")
-						t = (t+1)%4
-						
-					# This controls how often we read responses and update results
-					# If this value is too high we're opening too many streams without reading responses
-					# If this value is too low we're reading too often (not using stream multiplexing effectively due to single thread)
-					# This value (20) is eyeballed, it should be tested more thoroughly
-					if i>20:
-						found += dump_scan(requests)
-						requests.clear()
-						i = 0
+			for ex in ext:
+				# Prevent flood
+				time.sleep(0.05)
+				# Rotating bar
+				if int(time.time())%5==0:
+					print(l[t], end="\r")
+					t = (t+1)%4
+					
+				# This controls how often we read responses and update results
+				# If this value is too high we're opening too many streams without reading responses
+				# If this value is too low we're reading too often (not using stream multiplexing effectively due to single thread)
+				# This value (20) is eyeballed, it should be tested more thoroughly
+				if i>20:
+					found += dump_scan(requests)
+					requests.clear()
+					i = 0
 
-					# Don't need to scan entry.php/ so we skip it
-					if len(entry.split(".")[-1]) in ext and ex=="/": continue
-					# Perform request and store stream ID
-					sid = conn.request("HEAD", directory + entry + ex)
-					requests[directory + entry + ex] = sid
-					i += 1
+				# Don't need to scan entry.php/ so we skip it
+				if len(entry.split(".")[-1]) in ext and ex=="/": continue
+				# Perform request and store stream ID
+				sid = conn.request("HEAD", directory + entry + ex)
+				requests[directory + entry + ex] = sid
+				i += 1
 
-			found += dump_scan(requests)
+		found += dump_scan(requests)
 
-			# Recursively scan found directories
-			for fd in found:
-				print(" \n[*] Scanning " + fd)
-				recursive_dirscan(conn, fd, file, ext)
+		# Recursively scan found directories
+		for fd in found:
+			print(" \n[*] Scanning " + fd)
+			recursive_dirscan(conn, fd, file, ext)
 
-	if __name__ == "__main__":
+if __name__ == "__main__":
 
-		print("--------------------------------")
-		print("h2buster v" + __version__)
-		print("--------------------------------\n")
+	print("--------------------------------")
+	print("h2buster v" + __version__)
+	print("--------------------------------\n")
+
+	try:
 
 		# For benchmarking purposes
 		t0 = time.time()
@@ -143,18 +144,15 @@ try:
 
 		print("\n[*] Program ran in " + str(round(time.time()-t0, 3)) + " seconds")
 
-except ImportError:
-	print("[-] hyper missing: pip3 install hyper")
+	except OSError as ose:
+		if ose.errno == 113: print("[-] No route to host")
+		else: print(ose)
 
-except OSError as ose:
-	if ose.errno == 113: print("[-] No route to host")
-	else: print(ose)
+	except FileNotFoundError:
+		print("[-] File not found")
 
-except FileNotFoundError:
-	print("[-] File not found")
+	except AssertionError:
+		print("[-] H2 not supported for that target")
 
-except AssertionError:
-	print("[-] H2 not supported for that target")
-
-except KeyboardInterrupt:
-	print("\r[-] Scan aborted")
+	except KeyboardInterrupt:
+		print("\r[-] Scan aborted")
